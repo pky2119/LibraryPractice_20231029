1a8bd1a18e4915e1f9ba0ccbd7465181b9745803
pky2119 <pky2119@naver.com>
2023-10-29 15:20:12 +0900

    TedPermission 추가 + 권한 여부에 따른 행동 지침만 우선 작성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,5 +49,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation ("com.github.chrisbanes:PhotoView:2.3.0")
+    implementation ("io.github.ParkSangGwon:tedpermission-normal:3.3.0")
 
 }
--- app/src/main/java/kr/ac/wku/librarypractice_20231029/MainActivity.kt
+++ app/src/main/java/kr/ac/wku/librarypractice_20231029/MainActivity.kt
@@ -2,10 +2,13 @@ package kr.ac.wku.librarypractice_20231029
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.gun0912.tedpermission.PermissionListener
 import kr.ac.wku.librarypractice_20231029.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,14 +29,36 @@ class MainActivity : AppCompatActivity() {
 //             전화를 바로 걸기 ( 번호만 눌린 화면 X 신호까지 가도록)
 //             바로 걸기 (CALL) => 사용자 요금 사용 : 권한 허가 후 사용 가능
 
-            val myUri = Uri.parse("tel:010-1111-2222")
+            // 권한 획득 성공 여부에 따른 행동 지침을 변수에 저장 (인터페이스 => 객체화 : 익명 클래스 활용)
 
-            val myIntent = Intent(Intent.ACTION_CALL, myUri)
-            startActivity(myIntent)
+            val pl = object : PermissionListener{
+                override fun onPermissionGranted() {
+
+                    // 권한 허가 되었을 때 할 일 => 전화 걸기
+
+                    val myUri = Uri.parse("tel:010-1111-2222")
+
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                    // 거절되면 => 토스트로 안내
+                    Toast.makeText(this@MainActivity, "권한 문제로 저노하 연결이 안됩니다", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+
+
+
+
+
+            }
 
         }
 
 
 
     }
-}
