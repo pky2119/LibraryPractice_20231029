7b71586a7435d3a52056e1b2671d9e0411a7cfef
pky2119 <pky2119@naver.com>
2023-10-29 15:11:05 +0900

    전화 연결(CALL) 시도 권한문제로 앱 죽음

--- app/src/main/java/kr/ac/wku/librarypractice_20231029/MainActivity.kt
+++ app/src/main/java/kr/ac/wku/librarypractice_20231029/MainActivity.kt
@@ -1,6 +1,7 @@
 package kr.ac.wku.librarypractice_20231029
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
@@ -18,9 +19,19 @@ class MainActivity : AppCompatActivity() {
         binding.imgProfile.setOnClickListener{
             val myIntent = Intent(this, ViewPhotoActivity::class.java)
             startActivity(myIntent)
-            
+
         }
 
+        binding.btnCall.setOnClickListener {
+//             전화를 바로 걸기 ( 번호만 눌린 화면 X 신호까지 가도록)
+//             바로 걸기 (CALL) => 사용자 요금 사용 : 권한 허가 후 사용 가능
+
+            val myUri = Uri.parse("tel:010-1111-2222")
+
+            val myIntent = Intent(Intent.ACTION_CALL, myUri)
+            startActivity(myIntent)
+
+        }
 
 
 
